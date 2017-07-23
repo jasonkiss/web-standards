efe4f1cff770bd79d6b678ff269dafb25ed915d3
Jason Kiss <jason@accessibleculture.org>
2017-07-23 15:08:29 +1200

    /common/ -> /assets/

--- guidance/assessment-guides/js/fix-asset-urls.js
+++
@@ -1,7 +0,0 @@
-//add "/web-standards/" to beginning of every src that starts with "/assets/"
-$(document).ready(function(){
-	//get all <a> elements with @src="/assets/..."
-	$("img[src^='/assets/']").each(function() {
-		$(this).attr("src", "/web-standards/" + $(this).attr("src"));
-	});
-});
